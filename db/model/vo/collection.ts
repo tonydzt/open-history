326dff6bfdbdb0300797d28861d7a04a0cf4ac6d
douzhitong <douzhitong@pwrd.com>
2025-12-17 14:52:08 +0800

    用户登录后，自动创建默认收藏夹

--- db/model/vo/collection.ts
+++ db/model/vo/collection.ts
@@ -1,4 +1,5 @@
 import { CollectionEvent } from './collectionEvent';
+import { DEFAULT_COLLECTION_PREFIX } from '@/lib/constants';
 
 /**
  * 收藏夹模型接口
@@ -31,3 +32,13 @@ export function transformCollection(dbCollection: any): Collection {
     collection_event: dbCollection.collection_event
   };
 }
+
+/**
+ * 获取用户默认收藏夹ID
+ * @param userId 用户ID
+ * @returns 默认收藏夹ID，格式为`${DEFAULT_COLLECTION_PREFIX}${userId}`
+ */
+export function getDefaultCollectionId(userId: string): string {
+  return `${DEFAULT_COLLECTION_PREFIX}${userId}`;
+}
+
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+// 全局常量定义文件
+
+/**
+ * 默认收藏夹名
+ */
+export const DEFAULT_COLLECTION_PREFIX: string = 'default_collection_';
